d70e87952220b8e930c35e0eeeff34f9d8203921
shubhamaryal <aryalshubham62@gmail.com>
2025-04-29 13:09:46 +0545

    make the sidebar ui

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { LucideChartBarDecreasing } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface Product {
@@ -54,6 +55,62 @@ const Sidebar = () => {
   return (
     <div className="w-64 p-5 h-screen">
       <h1 className="text-2xl font-bold mb-10 mt-4">React Store</h1>
+      <section>
+        <input
+          type="text"
+          className="border-2 rounded px-2 sm:mb-0"
+          placeholder="Search Products"
+        />
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="min"
+          />
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="max"
+          />
+        </div>
+
+        {/* Categories Section */}
+        <div className="mb-5">
+          <h2 className="text-xl font-semibold mb-3">Categories</h2>
+        </div>
+        <section>
+          {categories.map((category, index) => (
+            <label key={index} className="block mb-2 ">
+              <input
+                type="radio"
+                name="category"
+                value={category}
+                className="mr-2 w-[16px] h-[16px]"
+              />
+              {category.toUpperCase()}
+            </label>
+          ))}
+        </section>
+
+        {/* Keyword Section */}
+        <div className="mb-5 mt-4">
+          <h2 className="text-xl font-semibold mb-3">Keywords</h2>
+          <div>
+            {keywords.map((keyword, index) => (
+              <button
+                key={index}
+                className="block mb-2 px-4 py-2 w-full text-left border rounded hover:bg-gray-200"
+              >
+                {keyword.toUpperCase()}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <button className="w-full mb-[4rem] py-2 bg-black text-white rounded mt-5">
+          Reset Filters
+        </button>
+      </section>
     </div>
   );
 };
